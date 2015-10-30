42196d06a7f6645e4495c55ceacbf440176b2db2
Sven Osterwalder <sven.osterwalder@gmail.com>
2015-10-30 21:41:08 +0100

    Include new dependencies on shader factory and shader model

--- src/main.hpp
+++ src/main.hpp
@@ -1,12 +1,21 @@
 # pragma once
 
 // Standard libraries
+#include <map>
 #include <memory>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <strstream>
+#include <sys/time.h>
 
 // Third-party libraries
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Own includes
 #include "fatal_exception.hpp"
+// #include "shader.hpp"
+#include "shader_factory.hpp"
