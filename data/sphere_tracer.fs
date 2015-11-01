863976de6a23e4258492ad550067a3da53e5388b
Sven Osterwalder <sven.osterwalder@gmail.com>
2015-11-01 18:52:35 +0100

    Clean up the code, add possibility to see distance to objects

--- data/sphere_tracer.fs
+++ data/sphere_tracer.fs
@@ -3,6 +3,8 @@
 uniform vec2 globalResolution;
 uniform float globalTime;
 
+#define SHOW_DISTANCE false
+
 mat3 calcLookAtMatrix(vec3 origin, vec3 target, float roll) {
     vec3 rr = vec3(sin(roll), cos(roll), 0.0);
     vec3 ww = normalize(target - origin);
@@ -53,88 +55,77 @@ float box(vec3 position, vec3 dimension)
     return max(max(position.x, position.y), position.z);
 }
 
+float ruler(in vec3 position, in float rulerOffset = 0.01)
+{
+    return abs(position.y) - rulerOffset;
+}
+
+
 float subtract(float a, float b)
 {
     return max(-b, a);
 }
 
-vec2 union(vec2 a, vec2 b)
+float union(float a, float b)
 {
-    return (a.x < b.x) ? a : b;
+    return (a < b) ? a : b;
 }
 
-vec2 intersect(vec2 a, vec2 b)
+float intersect(float a, float b)
 {
-    return (a.x > b.x) ? a : b;
+    return (a > b) ? a : b;
 }
 
-vec2 scene(in vec3 position)
+float scene1(in vec3 position)
 {
-    float planeId   = 0.0;
-    float sphereId  = 1.0;
-    float boxId     = 2.0;
-    float sphere2Id = 3.0;
-    float sphere3Id = 4.0;
-
     float sphereRadius   = 0.6;
     float sphere3Radius  = 1.0;
     vec3  boxDimension   = vec3(0.8);
 
     vec3 sphereOffset  = vec3(-2.0, 1.0, -2.0);
-    vec3 boxOffset     = vec3(-3.0, 1.0,  0.0);
+    vec3 boxOffset     = vec3(-3.0, 0.7,  0.0);
     vec3 box2Offset    = vec3(-2.0, 1.0, -2.0);
 
-    vec2 res = union(
-        vec2(plane(position), planeId),
-        vec2(sphere(position - sphereOffset, sphereRadius), sphereId)
+    float intersection = intersect(
+        box(position - boxOffset, vec3(0.8)),
+        sphere(position - boxOffset, 1.1)
     );
 
-    vec2 intersection = intersect(
-        vec2(box(position - boxOffset, vec3(0.8)), boxId),
-        vec2(sphere(position - boxOffset, 1.1), sphere2Id)
-    );
-
-    vec2 subtraction = vec2(subtract(
+    float subtraction = subtract(
         box(position - box2Offset, vec3(0.8)),
         sphere(position - box2Offset, sphere3Radius)
-    ), sphere3Id);
+    );
 
-    res = union(
-        res,
+    float result = union(
+        sphere(position - sphereOffset, sphereRadius),
         intersection
     );
 
-    res = union(
-        res,
+    result = union(
+        result,
         subtraction
     );
 
-    return res;
+    return result;
 }
 
-vec2 testScene(in vec3 position)
+float testScene(in vec3 position)
 {
-    float sphereId      = 0.0;
-    float sphere2Id     = 1.0;
-    float sphereRadius  = 1.0;
-    vec3  sphereOffset  = vec3(-2.0, 1.0, -2.0);
-    vec3  sphere2Offset = vec3(2.5, 1.0,  -2.0);
+    float result = sphere(position - vec3(0.0, 0.0, 0.0), 1.0);
 
-    vec2 res = union(
-        vec2(
-            sphere(position - sphereOffset, sphereRadius),
-            sphereId
-        ),
-        vec2(
-            sphere(position - sphere2Offset, sphereRadius),
-            sphere2Id
-        )
-    );
-
-    return res;
+    return result;
 }
 
-vec3 calcLighting(vec3 normal, vec3 rayDirection) {
+vec3 combineScenes(vec3 p)
+{
+    float scene     = scene1(p);
+    float ruler     = ruler(p);
+    float showRuler = ruler < scene ? 1.0 : 0.0;
+
+    return vec3(min(scene, ruler), showRuler, scene);
+}
+
+vec3 calcLighting(vec3 normal, vec3 rayDirection, in vec3 material) {
 
     vec3 lightDirection     = normalize(vec3(0.0, 4.0, 5.0));
 
@@ -153,59 +144,134 @@ vec3 calcLighting(vec3 normal, vec3 rayDirection) {
     float kSpecular         = pow(clamp(dot(h, normal), 0.0, 1.0), kSpecularExponent);
     vec3 specular           = nFacing * kSpecular * specularColor;
 
-    vec3 color              = ambient + diffuse + specular;
+    vec3 light              = ambient + diffuse + specular;
+    vec3 color              = material * light;
 
     return color;
 }
 
-vec3 calcNormal(in vec3 position, in float eps) {
-
-    vec3 epsilon = vec3(eps, 0.0, 0.0);
-    vec3 normal  = vec3(
-        scene(position + epsilon.xyy).x - scene(position - epsilon.xyy).x,
-        scene(position + epsilon.yxy).x - scene(position - epsilon.yxy).x,
-        scene(position + epsilon.yyx).x - scene(position - epsilon.yyx).x
-    );
-
-    return normalize(normal);
+vec3 calcMaterial(in vec3 position, in vec3 normal)
+{
+    return vec3(0.6);
 }
 
-vec2 castRay(in vec3 rayOrigin, in vec3 rayDirection, in float
-        maxDistance, in float precision, in int steps)
+vec3 calcNormal(in vec3 position, in float epsilon)
 {
-    float latest    = precision * 2.0;
-    float distance  = 0.0;
-    float type      = -1.0;
-    vec2  res       = vec2(-1.0, -1.0);
+    const vec3 v1 = vec3( 1.0, -1.0, -1.0);
+    const vec3 v2 = vec3(-1.0, -1.0,  1.0);
+    const vec3 v3 = vec3(-1.0,  1.0, -1.0);
+    const vec3 v4 = vec3( 1.0,  1.0,  1.0);
+
+    vec3 sceneAtV1 = combineScenes(position + v1 * epsilon);
+    vec3 sceneAtV2 = combineScenes(position + v2 * epsilon);
+    vec3 sceneAtV3 = combineScenes(position + v3 * epsilon);
+    vec3 sceneAtV4 = combineScenes(position + v4 * epsilon);
+
+    float currentDistanceAtV1 = -1.0;
+    float currentDistanceAtV2 = -1.0;
+    float currentDistanceAtV3 = -1.0;
+    float currentDistanceAtV4 = -1.0;
+
+
+    if (SHOW_DISTANCE) {
+        currentDistanceAtV1 = sceneAtV1.x;
+        currentDistanceAtV2 = sceneAtV2.x;
+        currentDistanceAtV3 = sceneAtV3.x;
+        currentDistanceAtV4 = sceneAtV4.x;
+    }
+    else {
+        currentDistanceAtV1 = sceneAtV1.z;
+        currentDistanceAtV2 = sceneAtV2.z;
+        currentDistanceAtV3 = sceneAtV3.z;
+        currentDistanceAtV4 = sceneAtV4.z;
+    }
+
+    return normalize(v1 * currentDistanceAtV1 +
+                     v2 * currentDistanceAtV2 +
+                     v3 * currentDistanceAtV3 +
+                     v4 * currentDistanceAtV4);
+}
+
+float calcRule(in float currentDistance, in float scale) {
+    return mix(1.0, 0.35, smoothstep(0.6, 1.0, abs(fract(currentDistance * scale) * 2.0 - 1.0)));
+}
+
+vec3 calcRulerColor(float stepSize) {
+    stepSize = clamp(log(stepSize + 1.0), 0.0, 1.0);
+
+    return mix(mix(vec3(0.0, 0.1, 1.0), vec3(1.0, 0.1, 0.0), stepSize * 5.0), vec3(1.0), smoothstep(0.2, 0.5, stepSize));
+}
+
+vec3 calcRulerMaterial(float distanceToScene, float currentDistance) {
+    float limit = log(currentDistance) / log(10.0);
+    float stepSize = pow(10.0, -floor(limit));
+    float scale = smoothstep(0.0, 0.33, fract(limit));
+    float interpolatedRule = calcRule(distanceToScene, stepSize * 10.0) * mix(calcRule(distanceToScene, stepSize * 100.0), calcRule(distanceToScene, stepSize), scale);
+
+    return mix(calcRulerColor(stepSize * distanceToScene), calcRulerColor(stepSize * distanceToScene * 0.1), scale) * 0.8 * interpolatedRule;
+}
+
+vec3 castRay(in vec3 rayOrigin, in vec3 rayDirection, in float maxDistance, in float precision, in int steps)
+{
+    float latest          = precision * 2.0;
+    float currentDistance = 0.0;
+    float result          = -1.0;
+    vec3  ray             = vec3(0);
+    float ruler           = 0.0;
+    vec3 combinedScenes   = vec3(-1.0);
 
     for(int i = 0; i < steps; i++) {
-        if (abs(latest) < precision || distance > maxDistance) {
+        if (abs(latest) < precision || currentDistance > maxDistance) {
             continue;
         }
 
-        vec2 result = scene(rayOrigin + rayDirection * distance);
+        ray = rayOrigin + rayDirection * currentDistance;
+        combinedScenes = combineScenes(ray);
 
-        latest     = result.x;
-        type       = result.y;
-        distance  += latest;
+        if (SHOW_DISTANCE) {
+            latest = combinedScenes.x;
+        }
+        else {
+            latest = combinedScenes.z;
+        }
+
+        currentDistance += latest;
     }
 
-    if (distance < maxDistance) {
-        res = vec2(distance, type);
+    if (currentDistance < maxDistance) {
+        result = currentDistance;
     }
 
-    return res;
+    float showRuler     = combinedScenes.y;
+    float renderedScene = combinedScenes.z;
+
+    return vec3(result, showRuler, renderedScene);
 }
 
 vec3 render(in vec3 rayOrigin, in vec3 rayDirection)
 {
-    vec3 color = vec3(0.015);
-    vec2 res   = castRay(rayOrigin, rayDirection, 100.0, 0.00001, 100);
+    vec3  color           = vec3(0.0);
+    vec3  res             = castRay(rayOrigin, rayDirection, 100.0, 0.00001, 100);
+    float currentDistance = res.x;
+    bool  showRuler       = res.y == 1.0;
+    float renderedScene   = res.z;
 
-    if (res.x > -0.5) {
-        vec3 position = rayOrigin + res.x * rayDirection;
-        vec3 normal   = calcNormal(position, 0.1);
-        color         = calcLighting(normal, rayDirection);
+    if (currentDistance > -0.5) {
+        vec3 position = rayOrigin + currentDistance * rayDirection;
+        vec3 normal   = calcNormal(position, 0.000001);
+
+        vec3 material = vec3(0.8);
+
+        if (SHOW_DISTANCE) {
+            if (showRuler) {
+                material = calcRulerMaterial(renderedScene, currentDistance);
+            }
+            else {
+                material = calcMaterial(position, normal);
+            }
+        }
+
+        color = calcLighting(normal, rayDirection, material);
     }
 
     vec3 gamma = vec3(1.0 / 2.2);
