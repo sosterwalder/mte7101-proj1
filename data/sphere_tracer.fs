0b12032c6e6df3de9fb6192866b8358935ef4d6e
Sven Osterwalder <sven.osterwalder@gmail.com>
2015-11-15 16:34:13 +0100

    Prototype: Add new lighting method, change scene a bit

--- data/sphere_tracer.fs
+++ data/sphere_tracer.fs
@@ -88,11 +88,11 @@ float scene1(in vec3 position)
     float sphere3Radius  = 1.0;
     vec3  boxDimension   = vec3(0.8);
 
-    vec3 sphereOffset  = vec3(-2.0, 1.0, -2.0);
-    vec3 boxOffset     = vec3(-3.0, 0.7,  0.0);
-    vec3 box2Offset    = vec3(-2.0, 1.0, -2.0);
+    vec3 sphereOffset  = vec3(-2.0, 0.8, -2.0);
+    vec3 boxOffset     = vec3(-3.0, 0.8,  0.0);
+    vec3 box2Offset    = vec3(-2.0, 0.8, -2.0);
 
-    float plane = plane(position);
+    float plane = plane(position - vec3(-1.0));
 
     float intersection = intersect(
         box(position - boxOffset, vec3(0.8)),
@@ -133,19 +133,21 @@ float testScene(in vec3 position)
 {
     float result = 0.0;
 
+    float sphere = sphere(position - vec3(0.0, 1.0, 0.0), 1.0);
     float plane = plane(position);
-    float sphere = sphere(position - vec3(0.0, 0.5, 0.0), 1.0);
 
     result = merge(
         plane, sphere
     );
+
+    // result = sphere;
 
     return result;
 }
 
 vec3 combineScenes(vec3 p)
 {
-    float scene     = testScene(p);
+    float scene     = scene1(p);
     float ruler     = ruler(p, 0.01);
     float showRuler = ruler < scene ? 1.0 : 0.0;
 
@@ -155,7 +157,7 @@ vec3 combineScenes(vec3 p)
 float calcShadows(in vec3 rayOrigin, in vec3 rayDirection)
 {
     float shadow            = 1.0;
-    float minimalDistance   = 0.01;
+    float minimalDistance   = 0.02;
     float maximalDistance   = 2.5;
     float convergePrecision = 0.000001;
     float kShadow           = 8.0;
@@ -163,7 +165,7 @@ float calcShadows(in vec3 rayOrigin, in vec3 rayDirection)
 
     while (currentDistance < maximalDistance) {
         vec3 ray = rayOrigin + rayDirection * currentDistance;
-        float estimatedDistance = testScene(ray);
+        float estimatedDistance = scene1(ray);
 
         if (estimatedDistance < convergePrecision) {
             return 0.0;
@@ -174,42 +176,87 @@ float calcShadows(in vec3 rayOrigin, in vec3 rayDirection)
         currentDistance += estimatedDistance;
     }
 
-    return shadow;
+    return clamp(shadow, 0.0, 1.0);
 
+}
+
+vec3 calcBRDFLighting(in vec3 position, in vec3 normal, in vec3 rayDirection, in vec3 material, in vec3 lightPosition, in vec3 lightColor, in float currentDistance)
+{
+    vec3 lightDirection     = normalize(lightPosition);
+    vec3 reflection         = reflect(rayDirection, normal);
+
+    lightColor              = 0.45 + 0.3 * sin(vec3(0.05, 0.08, 0.10) * (currentDistance - 1.0));
+
+    if (currentDistance < 1.5) {
+        float f = mod(floor(5.0 * lightDirection.z) + floor(5.0 * lightDirection.x), 2.0);
+        lightColor = 0.4 + 0.1 * f * vec3(1.0);
+    }
+
+    vec3  ambientColor      = vec3(0.5, 0.7, 1.0);
+    vec3  diffuseColor      = vec3(1.0, 0.9, 0.6);
+    vec3  specularColor     = vec3(1.0, 0.9, 0.6);
+    vec3  domColor          = vec3(0.5, 0.7, 1.0);
+    vec3  bacColor          = vec3(0.25, 0.25, 0.25);
+    vec3  fresnelColor      = vec3(1.0);
+
+    float kOcclusion        = 0.0;
+    float kAmbient          = clamp(0.5 + 0.5 * normal.y, 0.0, 1.0);
+    float kDiffuse          = clamp(dot(lightDirection, normal), 0.0, 1.0);
+    float kBac              = clamp(dot(normal, normalize(vec3(-lightDirection.x, 0.0, -lightDirection.z))), 0.0, 1.0) * clamp(1.0 - position.y, 0.0, 1.0);
+    float kDom              = smoothstep(-0.1, 0.1, reflection.y);
+    float kFresnel          = pow(clamp(1.0 + dot(normal, rayDirection), 0.0, 1.0), 2.0);
+    float kSpecular         = pow(clamp(dot(reflection, lightDirection), 0.0, 1.0), 16.0);
+
+    kDiffuse               *= calcShadows(position, lightDirection);
+    kDom                   *= calcShadows(position, reflection);
+
+    vec3 brdf               = vec3(0.0);
+    brdf                   += 1.20 * kDiffuse  * diffuseColor;
+    brdf                   += 1.20 * kSpecular * specularColor * kDiffuse;
+    brdf                   += 0.30 * kAmbient  * ambientColor  * kOcclusion;
+    brdf                   += 0.40 * kDom      * domColor      * kOcclusion;
+    brdf                   += 0.30 * kBac      * bacColor      * kOcclusion;
+    brdf                   += 0.40 * kFresnel  * fresnelColor  * kOcclusion;
+    brdf                   += 0.02;
+
+    lightColor             = lightColor * brdf * material;
+    lightColor             = mix(lightColor, vec3(0.8, 0.9, 1.0), 1.0 - exp(-0.0005 * currentDistance * currentDistance));
+
+    return lightColor;
 }
 
 vec3 calcLighting(in vec3 position, in vec3 normal, in vec3 rayDirection, in vec3 material, in vec3 lightPosition, in vec3 lightColor)
-{
-    vec3 lightDirection     = normalize(lightPosition);
-
-    float kDirectLight      = 0.1;
-    float shadows           = calcShadows(position, lightDirection);
-    vec3  direct            = vec3(kDirectLight * shadows);
-
-    vec3  ambientColor      = vec3(0.3, 0.3, 0.3);
-    float kAmbient          = clamp(0.5 + 0.5 * normal.y, 0.0, 1.0);
-    vec3  ambient           = kAmbient * ambientColor;
-
-    vec3  diffuseColor      = vec3(lightColor);
-    float kDiffuse          = clamp(dot(lightDirection, normal), 0.0, 1.0);
-    vec3  diffuse           = kDiffuse * diffuseColor;
-
-    vec3  specularColor     = vec3(1.0);
-    float kSpecularExponent = 24.0;
-    vec3  h                 = normalize(-rayDirection + lightDirection);
-    float nFacing           = clamp(dot(lightDirection, normal), 0.0, 1.0);
-    float kSpecular         = pow(clamp(dot(h, normal), 0.0, 1.0), kSpecularExponent);
-    vec3  specular          = nFacing * kSpecular * specularColor;
-
-    vec3 light              = ambient + diffuse + specular + direct;
-    vec3 color              = material * light;
-
-    return color;
-}
+ {
+     vec3 lightDirection     = normalize(lightPosition);
+ 
+     float kDirectLight      = 0.1;
+     float shadows           = calcShadows(position, lightDirection);
+     vec3  direct            = vec3(kDirectLight * shadows);
+ 
+     vec3  ambientColor      = vec3(0.3, 0.3, 0.3);
+     float kAmbient          = clamp(0.5 + 0.5 * normal.y, 0.0, 1.0);
+     vec3  ambient           = kAmbient * ambientColor;
+ 
+     vec3  diffuseColor      = vec3(lightColor);
+     float kDiffuse          = clamp(dot(lightDirection, normal), 0.0, 1.0);
+     vec3  diffuse           = kDiffuse * diffuseColor;
+ 
+     vec3  specularColor     = vec3(1.0);
+     float kSpecularExponent = 24.0;
+     vec3  h                 = normalize(-rayDirection + lightDirection);
+     float nFacing           = clamp(dot(lightDirection, normal), 0.0, 1.0);
+     float kSpecular         = pow(clamp(dot(h, normal), 0.0, 1.0), kSpecularExponent);
+     vec3  specular          = nFacing * kSpecular * specularColor;
+ 
+     vec3 light              = ambient + diffuse + specular + direct;
+     vec3 color              = material * light;
+ 
+     return color;
+ }
 
 vec3 calcMaterial(in vec3 position, in vec3 normal)
 {
-    return vec3(0.2);
+    return vec3(1.0);
 }
 
 vec3 calcNormal(in vec3 position, in float epsilon)
@@ -275,19 +322,17 @@ vec3 calcPostFx(vec3 color, vec2 screenPosition)
     float brightness = 1.2;
 
     // Gamma correction
-    color = pow(color, vec3(0.45));
+    color = pow(color, vec3(0.4545));
 
-    color = mix(vec3(.5), mix(vec3(dot(vec3(.2125, .7154, .0721),
-                        color*brightness)), color*brightness, saturation), contrast);
+    // color = mix(vec3(.5), mix(vec3(dot(vec3(.2125, .7154, .0721), color*brightness)), color*brightness, saturation), contrast);
 
     // Noise
-    float noiseFactor = hash(globalTime);
-    vec2 noise        = noiseFactor * screenPosition;
+    // float noiseFactor = hash(globalTime);
+    // vec2 noise        = noiseFactor * screenPosition;
     // color             = clamp(color + noise * 0.1, 0.0, 1.0);
 
     // Vignetting
-    // color *=
-    // .4+0.5*pow(40.0*screenPosition.x*screenPosition.y*(1.0-screenPosition.x)*(1.0-screenPosition.y), 0.2 );
+    // color *= .4+0.5*pow(40.0*screenPosition.x*screenPosition.y*(1.0-screenPosition.x)*(1.0-screenPosition.y), 0.2 );
 
     return color;
 }
@@ -331,7 +376,7 @@ vec3 castRay(in vec3 rayOrigin, in vec3 rayDirection, in float maxDistance, in f
 
 vec3 render(in vec3 rayOrigin, in vec3 rayDirection)
 {
-    vec3  color           = vec3(0.0);
+    vec3  color           = vec3(0.05, 0.08, 0.10);
     vec3  res             = castRay(rayOrigin, rayDirection, 100.0, 0.00001, 100);
     float currentDistance = res.x;
     bool  showRuler       = res.y == 1.0;
@@ -356,18 +401,17 @@ vec3 render(in vec3 rayOrigin, in vec3 rayDirection)
         }
 
         vec3 light1Color = vec3(0.7, 0.2, 0.3);
-        vec3 light1Position = vec3(1.3, 1.7, 5.0);
+        vec3 light1Position = vec3(-0.6, 0.7, -0.5);
+        // vec3 light1 = calcBRDFLighting(position, normal, rayDirection, material, light1Position, light1Color, currentDistance);
         vec3 light1 = calcLighting(position, normal, rayDirection, material, light1Position, light1Color);
 
-        // vec3 light2Color = vec3(0.3, 0.2, 0.8);
-        // vec3 light2Position = vec3(1.3, 0.2, 0.8);
-        // vec3 light2 = calcLighting(position, normal, rayDirection, material, light2Position, light2Color);
+        vec3 light2Color = vec3(0.3, 0.2, 0.8);
+        vec3 light2Position = vec3(1.3, 0.2, 0.8);
+        // vec3 light2 = calcBRDFLighting(position, normal, rayDirection, material, light2Position, light2Color, currentDistance);
+        vec3 light2 = calcLighting(position, normal, rayDirection, material, light2Position, light2Color);
 
-        color = light1; // + light2;
+        color = light1 + light2;
     }
-
-    vec3 gamma = vec3(1.0 / 2.2);
-    color      = pow(color, gamma);
     color      = clamp(color, 0.0, 1.0);
 
     return color;
@@ -378,17 +422,17 @@ void main()
     vec2 resolution      = globalResolution;
     float time           = 1.0; // globalTime * 1.1;
     float cameraAngle    = 1.0;
-    float cameraHeight   = 5.0;
-    float cameraPane     = 3.0;
-    float cameraDistance = 2.5;
+    float cameraHeight   = 2.0;
+    float cameraPane     = 4.0;
+    float cameraDistance = 4.5;
     vec3 rayOrigin      = vec3(cameraPane * sin(cameraAngle), cameraHeight, cameraDistance * cos(cameraAngle * time));
-    vec3 rayTarget      = vec3(-1.0, -5.0, -3.0);
+    vec3 rayTarget      = vec3(0.0, 0.0, 0.0);
     vec2 screenPosition = squareFrame(resolution);
     vec3 rayDirection   = getRay(rayOrigin, rayTarget, screenPosition, 2.0);
 
     vec3 color          = render(rayOrigin, rayDirection);
     color               = calcPostFx(color, screenPosition);
 
-    gl_FragColor.rgb = color;
-    gl_FragColor.a   = 1.0;
+    gl_FragColor.rgb    = color;
+    gl_FragColor.a      = 1.0;
 }
